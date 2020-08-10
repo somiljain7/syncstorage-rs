be436af9d907523ea60d7f07299ab3d1a1c9d2c2
JR Conlin <jrconlin@gmail.com>
2020-08-10 11:45:25 -0700

    bug: Return WeaveError::OverQuota for over quota responses (#773)
    Closes #769

--- src/web/extractors.rs
+++ src/web/extractors.rs
@@ -236,9 +236,9 @@ impl FromRequest for BsoBodies {
                             error!("Returning over quota for {:?}", v);
                             return Box::pin(future::err(
                                 ValidationErrorKind::FromDetails(
-                                    "size-limit-exceeded".to_owned(),
+                                    "over-quota".to_owned(),
                                     RequestErrorLocation::Unknown,
-                                    Some("size-limit-exceeded".to_owned()),
+                                    Some("over-quota".to_owned()),
                                     None,
                                 )
                                 .into(),
@@ -416,6 +416,33 @@ impl FromRequest for BsoBody {
                 ));
             }
         };
+
+        // ### debug_client
+        if let Some(uids) = &state.limits.debug_client {
+            for uid in uids.split(',') {
+                debug!("### checking uaid: {:?}", &uid);
+                match u64::from_str(uid.trim()) {
+                    Ok(v) => {
+                        if v == HawkIdentifier::uid_from_path(req.uri(), None).unwrap_or(0) {
+                            debug!("### returning quota exceeded.");
+                            error!("Returning over quota for {:?}", v);
+                            return Box::pin(future::err(
+                                ValidationErrorKind::FromDetails(
+                                    "over-quota".to_owned(),
+                                    RequestErrorLocation::Unknown,
+                                    Some("over-quota".to_owned()),
+                                    None,
+                                )
+                                .into(),
+                            ));
+                        }
+                    }
+                    Err(_) => {
+                        debug!("{:?} is not a u64", uid);
+                    }
+                };
+            }
+        }
 
         let max_payload_size = state.limits.max_record_payload_bytes as usize;
 
