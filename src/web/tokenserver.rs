5547d8f707c0f880d939d62e6475072d19e39d9e
Donovan Preston <donovanpreston@gmail.com>
2020-10-02 15:52:41 -0400

    wip

--- src/web/tokenserver.rs
+++ src/web/tokenserver.rs
@@ -23,11 +23,27 @@ pub struct TokenServerResult {
 
 use actix_web_httpauth::extractors::bearer::BearerAuth;
 
+
+#[derive(Debug, serde::Serialize, serde::Deserialize, PartialEq)]
+pub struct Claims {
+    pub sub: String,
+    pub company: String,
+    pub iat: i64,
+    pub exp: i64,
+}
+
 pub fn get(
     request: TokenServerRequest, auth: BearerAuth
 ) -> impl Future<Output = Result<HttpResponse, BlockingError<ApiError>>> {
     block(move || get_sync(request).map_err(Into::into)).map_ok(move |mut result| {
-        result.id = auth.token().to_string();
+        let pubkey_path = "".to_string();
+        //result.id = auth.token().to_string();
+        let claims = decode::<Claims>(
+            &auth.token(),
+            &DecodingKey::from_rsa_pem(std::fs::read(&pubkey_path).unwrap().as_slice()).unwrap(),
+            &Validation::new(Algorithm::RS256),
+        );
+
         let body = serde_json::to_string(&result).unwrap();
         println!("BODY! {:}", body);
         HttpResponse::Ok()
